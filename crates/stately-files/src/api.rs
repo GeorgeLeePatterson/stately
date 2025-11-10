17eaa288f48e7057570dd7854a866080169a5008
George Patterson <patterson.george@gmail.com>
2025-11-10 13:33:19 -0500

    fix(api): minor updates to stately files

--- crates/stately-files/src/api.rs
+++ crates/stately-files/src/api.rs
@@ -10,12 +10,28 @@ use crate::entity::VersionedPath;
 use crate::settings::Dirs;
 
 // TODO:
-//   * Add Convenience trait to blanket impl FromRef<S> for FilesState
-//   * Illustrate a FromRef<S> example
+//   * Discuss simple macro and trait below. Really not necessary, but may be helpful
 #[derive(Debug, Clone, PartialEq, Hash, Serialize, Deserialize)]
 pub struct FileState {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub base: Option<Dirs>,
+}
+
+/// Helper trait to leverage macro `from_ref` for `FromRef<S>` into`FileState`
+pub trait FileStateExt<S> {
+    fn as_file_state(other: &S) -> FileState;
+}
+
+/// Convenience macro for types that implement `FileStateExt` to implement `FromRef<S>`
+#[macro_export]
+macro_rules! from_ref {
+    ($state:ty) => {
+        impl ::axum::extract::FromRef<$state> for FileState {
+            fn from_ref(st: &S) -> $crate::api::FileState {
+                <$state as $crate::api::FileStateExt<$state>>::as_file_state(st)
+            }
+        }
+    };
 }
 
 /// `OpenAPI` documentation
